92d24b1ab42be007be32a574fd764f2a21554f0a
Lucas Correa <lucascorrea@Lucass-MacBook-Air.local>
2025-12-15 12:54:13 -0600

    Updated parameter 'name'

--- db/engine_call.py
+++ db/engine_call.py
@@ -1,9 +1,9 @@
 from sqlalchemy import create_engine
 
-def engine_call(user:str, password:str, host:str, port:str, database:str):
+def engine_call(user:str, password:str, host:str, port:str, name:str):
     """"This function creates an engine to connect to Postgres"""
 
     engine = create_engine(
-    f"postgresql://{user}:{password}@{host}:{port}/{database}"
+    f"postgresql://{user}:{password}@{host}:{port}/{name}"
     )
     return engine 
